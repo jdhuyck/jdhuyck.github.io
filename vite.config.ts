f53dde08a9c14a065cc914c79dddf6f0e17b9f64
Jody Huyck-Jones <68274088+jdhuyck@users.noreply.github.com>
2025-08-22 09:05:33 +0100

    Setup MDX

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mdx from '@mdx-js/rollup'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    mdx({
+      // MDX options
+    }),
+    react()
+  ],
   base: '/',
   build: {
     outDir: 'dist',
