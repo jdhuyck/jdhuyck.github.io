66440bce7323ad22ed90bcea9a64e7f75e73416c
Jody Huyck-Jones <68274088+jdhuyck@users.noreply.github.com>
2025-08-23 22:18:50 +0100

    build

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-export default defineConfig({
-    plugins: [
-        react()
-    ],
-    base: '/',
-    build: {
-        outDir: 'dist',
-    }
+export default defineConfig(function (_a) {
+    var mode = _a.mode;
+    var env = loadEnv(mode, process.cwd(), '');
+    return {
+        plugins: [
+            react()
+        ],
+        base: '/',
+        build: {
+            outDir: 'dist',
+        },
+        define: {
+            'process.env.VITE_GITHUB_CLIENT_ID': JSON.stringify(env.VITE_GITHUB_CLIENT_ID),
+            'process.env.VITE_GITHUB_CLIENT_SECRET': JSON.stringify(env.VITE_GITHUB_CLIENT_SECRET),
+            'process.env.VITE_ADMIN_PASSWORD': JSON.stringify(env.VITE_ADMIN_PASSWORD),
+        },
+    };
 });
